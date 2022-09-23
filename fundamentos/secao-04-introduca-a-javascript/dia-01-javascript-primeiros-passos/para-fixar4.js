769cdeb30ef846d4f70ffc2c1f213fe16e50e3a6
CMScofield7 <scoundreldays2@gmail.com>
2022-09-23 15:08:37 -0300

    Foguetinhos e Para Fixar do dia 1, Seção 4

--- fundamentos/secao-04-introduca-a-javascript/dia-01-javascript-primeiros-passos/para-fixar4.js
+++ fundamentos/secao-04-introduca-a-javascript/dia-01-javascript-primeiros-passos/para-fixar4.js
@@ -1,6 +1,23 @@
-const currentHour = 7;
-let message;
+const currentHour = 5;
+let message = "";
 
 if (currentHour >= 22) {
-    console.log("Não deveriamos comer nada, é hora de dormir!");
+    message = "Não deveriamos comer nada, é hora de dormir!";
+    console.log(message)
+}
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar. :D";
+    console.log(message)
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?"
+    console.log(message);
+}
+else if (currentHour >= 11 && currentHour <= 14) {
+    message = "Hora do almoço!!!"
+    console.log(message);
+}
+else if (currentHour >= 4 && currentHour <= 11) {
+    message = "Hmmmmm, cheiro de café recém-passado!"
+    console.log(message);
 }
